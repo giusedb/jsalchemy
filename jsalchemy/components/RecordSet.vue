01b5ead9ef60ff83d54b5195a966a9e7314ba33e
Giuseppe Di Bona <giusedb@gmail.com>
2025-12-10 00:31:50 +0100

    Improved `RecordSet` Vue component

--- jsalchemy/components/RecordSet.vue
+++ jsalchemy/components/RecordSet.vue
@@ -3,9 +3,7 @@ import { Orm } from "../orm.js";
 import 'lodash';
 import utils from '../utils.js';
 
-const emits = defineEmits(['loading', 'records']);
-const local = {recordSet: null};
-const iOrm = inject('orm');
+const emits = defineEmits(['loading', 'records', 'recordSet']);
 const props = defineProps({
   resource: String,
   filter: Object,
@@ -15,19 +13,17 @@ const props = defineProps({
   localOrm: { type: [Orm, null], default: null },
   name: String,
 });
-
+const local = {recordSet: null};
+const iOrm = inject('orm');
 const state = reactive({
   records: [],
   touchRecord: 0,
   total: 0,
   loading: false,
 });
-
-
 const orm = computed(() => {
   return props.localOrm || iOrm;
 });
-
 const total = computed(() => {
   return local.recordSet?.totalCount;
 });
@@ -38,10 +34,11 @@ onMounted(() => {
     rpp: props.recordsPerPage,
     sort: props.sort,
   }, props.name);
-  local.recordSet.on('results', (recs, totalCount) => {
+  local.recordSet.on('records', (recs, totalCount) => {
     if (recs.length === state.records.length) {
       for (let i = 0; i < recs.length; i ++) {
-        state.records[i] = recs[i];
+        if (recs[i])
+          state.records[i] = recs[i];
       }
     } else {
       state.records.length = 0;
@@ -56,24 +53,34 @@ onMounted(() => {
     if (state.page !== paging.page) {
       state.page = paging.page;
     }
-  })
+  });
+  emits('recordSet', local.recordSet);
 });
+onUnmounted(() => {
+  local.recordSet.destroy();
+})
 
 watch(() => props.page, (newVal, oldVal) => {
   if (newVal !== oldVal)
     local.recordSet.page = newVal;
 });
 watch(() => props.recordsPerPage, (newVal, oldVal) => {
-  if (newVal !== oldVal)
+  if (newVal !== oldVal) {
     local.recordSet.rpp = newVal;
+    local.recordSet.page = 1;
+  }
 });
 watch(() => props.filter, (newVal, oldVal) => {
-  if (JSON.stringify(newVal) !== JSON.stringify(oldVal))
+  if (JSON.stringify(newVal) !== JSON.stringify(oldVal)) {
     local.recordSet.filter = newVal;
+    local.recordSet.page = 1;
+  }
 });
 watch(() => props.sort, (newVal, oldVal) => {
-  if (JSON.stringify(newVal) !== JSON.stringify(oldVal))
+  if (JSON.stringify(newVal) !== JSON.stringify(oldVal)) {
     local.recordSet.sort = newVal;
+    local.recordSet.page = 1;
+  }
 });
 
 </script>
